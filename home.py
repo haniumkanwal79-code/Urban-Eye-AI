866af76827cdc2c6ba402cf6b5d199bdb1a856f2
haniumkanwal79-code <haniumkanwal79@gmail.com>
2026-06-06 15:39:46 -0400

    Update home.py

--- home.py
+++ home.py
@@ -10,10 +10,6 @@ def load_css():
     with open("style.css") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-import database as db
-
-db.create_table()
-
 # ================= MAIN FUNCTION =================
 def show_home():
 
