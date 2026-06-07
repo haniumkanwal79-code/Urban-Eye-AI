43f1669b0689a4eeb2390bc7622d307e1f3fea99
haniumkanwal79-code <haniumkanwal79@gmail.com>
2026-06-07 07:02:46 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,17 +1,18 @@
-
 import sqlite3
+import os
 
 DB_NAME = "issues.db"
 
 def get_connection():
-    return sqlite3.connect(DB_NAME, check_same_thread=False)
+    db_path = os.path.join(os.getcwd(), DB_NAME)
+    return sqlite3.connect(db_path, check_same_thread=False)
 
 def create_table():
 
     conn = get_connection()
     c = conn.cursor()
 
-    # ================= ISSUES TABLE =================
+    # ISSUES TABLE
     c.execute("""
         CREATE TABLE IF NOT EXISTS issues (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -23,7 +24,7 @@ def create_table():
         )
     """)
 
-    # ================= USERS TABLE =================
+    # USERS TABLE
     c.execute("""
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
